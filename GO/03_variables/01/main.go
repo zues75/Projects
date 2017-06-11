8e8fd3b70fe29d698d958cdb756692b426c9c3fd
David Reedy <whiseone@gmail.com>
2017-06-11 13:04:17 -0400

    next lesson

---
+++ GO/03_variables/01/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 10
+	b := "golang"
+	c := 4.17
+	d := true
+	fmt.Println("%v \n", a)
+	fmt.Println("%v \n", b)
+	fmt.Println("%v \n", c)
+	fmt.Println("%v \n", d)
+}
